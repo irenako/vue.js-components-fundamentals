de901aae37d95aeab6cc3e90307cba69872b0fbe
irenako <108225581+irenako@users.noreply.github.com>
2022-11-15 11:04:29 +0200

    Created slots

--- plan-picker/app.js
+++ plan-picker/app.js
@@ -1,5 +1,5 @@
-let PlanComponent = {
-  template: "#plan-template",
+let PlanPickerItemComponent = {
+  template: "#plan-picker-item-template",
   props: {
     name: {
       type: String,
@@ -25,7 +25,7 @@ let PlanComponent = {
 let PlanPickerComponent = {
   template: "#plan-picker-template",
   components: {
-    plan: PlanComponent,
+    PlanPickerItem: PlanPickerItemComponent,
   },
   data() {
     return {
